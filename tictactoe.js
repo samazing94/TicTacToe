9abb1a6249609761d67d908ad2ff01d20600e8e5
Sammy J <samina.jamil@outlook.com>
2017-02-08 09:43:15 +0600

    tictactoe class

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,33 @@
 //JS code goes here
+//JS code goes here
+var turn = true;
+var player = [];
+var clicked = [];
+function play(event){
+  var element = event.target;
+
+  if (!clicked[element.id]) {
+    clicked[element.id] = true;
+    console.log(clicked);
+    console.log(event);
+    if (player[1])
+    {
+      element.innerHTML = "X";
+    }
+    else if (player[2]){
+      element.innerHTML = "X";
+    }
+    element.innerHTML = turn ? "X" : "O";
+    turn = !turn;
+  }
+}
+
+function status (event) {
+
+}
+function restart (event){
+  var element = document.getElementById("clear");
+    for(var i = 0; i < clicked.length; i++){
+      clicked[i] = "";
+    }
+}
